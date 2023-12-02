4d740c8be49b1e539cfbcdf9f837870b64f22f9e
aisazadeh <aisazadeh@sayesaman.com>
2023-12-02 18:05:44 +0330

    sound recording and playing it to my self

--- app/src/main/java/com/test/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/test/audiorecorder/MainActivity.kt
@@ -1,20 +1,80 @@
 package com.test.audiorecorder
 
+import android.Manifest
+import android.app.Activity
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.media.projection.MediaProjectionManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import com.test.audiorecorder.service.MediaCaptureService
+import com.test.audiorecorder.service.MediaCaptureService.Companion.ACTION_START
+import com.test.audiorecorder.service.MediaCaptureService.Companion.ACTION_STOP
 import com.test.audiorecorder.ui.theme.AudioRecorderTheme
 
 class MainActivity : ComponentActivity() {
+
+
+    lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var mediaProjectionManager: MediaProjectionManager
+
+    companion object {
+        private const val MEDIA_PROJECTION_REQUEST_CODE = 13
+        private const val RECORD_AUDIO_PERMISSION_REQUEST_CODE = 42
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            // Handle the result here
+            if (result.resultCode == Activity.RESULT_OK) {
+                // Process the result data
+                val data: Intent? = result.data
+                Intent(applicationContext, MediaCaptureService::class.java).apply {
+                    this.action = ACTION_START
+                    this.putExtra(MediaCaptureService.EXTRA_RESULT_DATA , data)
+
+                    startService(this)
+                }
+            }
+        }
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.FOREGROUND_SERVICE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+            ),
+            0
+        )
+
+
+
+
         setContent {
             AudioRecorderTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +82,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(24.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+
+                            Button(onClick = {
+//                                Intent(applicationContext, MediaCaptureService::class.java).apply {
+//                                    this.action = ACTION_START
+//
+//                                    startService(this)
+//                                }
+
+                                mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+
+                                launcher.launch(mediaProjectionManager.createScreenCaptureIntent(),)
+
+
+                            }) {
+                                Text(text = "start", style = MaterialTheme.typography.bodyMedium)
+                            }
+
+                            Button(onClick = {
+                                Intent(applicationContext, MediaCaptureService::class.java).apply {
+                                    this.action = ACTION_STOP
+
+                                    stopService(this)
+                                }
+                            }) {
+                                Text(text = "stop", style = MaterialTheme.typography.bodyMedium)
+
+                            }
+
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/test/audiorecorder/service/MediaCaptureService.kt
+++ app/src/main/java/com/test/audiorecorder/service/MediaCaptureService.kt
@@ -13,39 +13,56 @@ import android.media.AudioAttributes
 import android.media.AudioFormat
 import android.media.AudioPlaybackCaptureConfiguration
 import android.media.AudioRecord
+import android.media.AudioTrack
+import android.media.MediaCodec
+import android.media.MediaCodecInfo
+import android.media.MediaFormat
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.Build
+import android.os.Environment
 import android.os.IBinder
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import java.io.BufferedOutputStream
+import java.io.ByteArrayInputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.sql.Time
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 import kotlin.concurrent.thread
 import kotlin.experimental.and
 
-class MediaCaptureService : Service(){
+class MediaCaptureService : Service() {
 
     private lateinit var mediaProjectionManager: MediaProjectionManager
     private var mediaProjection: MediaProjection? = null
 
     private lateinit var audioCaptureThread: Thread
     private var audioRecord: AudioRecord? = null
+    private var audioTrack: AudioTrack? = null
 
     override fun onCreate() {
         super.onCreate()
 
         createNotificationChannel()
 
-        startForeground(SERVICE_ID, NotificationCompat.Builder(this,
-            NOTIFICATION_CHANNEL_ID).build())
+        startForeground(
+            SERVICE_ID, NotificationCompat.Builder(
+                this,
+                NOTIFICATION_CHANNEL_ID
+            ).build()
+        )
 
         mediaProjectionManager = applicationContext.getSystemService(
-            Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+            Context.MEDIA_PROJECTION_SERVICE
+        ) as MediaProjectionManager
 
     }
 
@@ -53,8 +70,10 @@ class MediaCaptureService : Service(){
 
 
     private fun createNotificationChannel() {
-        val serviceChannel = NotificationChannel(NOTIFICATION_CHANNEL_ID,
-            "Audio Capture Service Channel", NotificationManager.IMPORTANCE_DEFAULT)
+        val serviceChannel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            "Audio Capture Service Channel", NotificationManager.IMPORTANCE_DEFAULT
+        )
 
         val manager = getSystemService(NotificationManager::class.java) as NotificationManager
         manager.createNotificationChannel(serviceChannel)
@@ -66,31 +85,43 @@ class MediaCaptureService : Service(){
                 ACTION_START -> {
                     mediaProjection = mediaProjectionManager.getMediaProjection(Activity.RESULT_OK, intent.getParcelableExtra(EXTRA_RESULT_DATA)!!) as MediaProjection
 
-                    startAudioCapture()
+                    Log.d(LOG_TAG,"start");
+                    startCapturingAndPlayingAudio()
+//
+//                    startAudioCapture()
                     Service.START_STICKY
                 }
+
                 ACTION_STOP -> {
+                    Log.d(LOG_TAG,"stop");
+
                     stopAudioCapture()
                     Service.START_NOT_STICKY
                 }
+
                 else -> throw IllegalArgumentException("Unexpected action received: ${intent.action}")
             }
         } else {
             Service.START_NOT_STICKY
         }
     }
-    private fun startAudioCapture() {
-        // TODO: add code for executing audio capture itself
-        val config = AudioPlaybackCaptureConfiguration.Builder(mediaProjection!!)
-            .addMatchingUsage(AudioAttributes.USAGE_MEDIA)
-            .build()
 
+
+    private fun startCapturingAndPlayingAudio() {
+        // Set the desired audio format and buffer size
         val audioFormat = AudioFormat.Builder()
             .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
-            .setSampleRate(8000)
-            .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+            .setSampleRate(44100)
+            .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
             .build()
 
+        val bufferSizeInBytes = AudioRecord.getMinBufferSize(
+            44100,
+            AudioFormat.CHANNEL_IN_STEREO,
+            AudioFormat.ENCODING_PCM_16BIT
+        )
+
+        // Create an AudioRecord instance for capturing audio
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.RECORD_AUDIO
@@ -107,15 +138,92 @@ class MediaCaptureService : Service(){
         }
         audioRecord = AudioRecord.Builder()
             .setAudioFormat(audioFormat)
+            .setBufferSizeInBytes(bufferSizeInBytes)
+            .build()
+
+        // Create an AudioTrack instance for playing audio
+        audioTrack = AudioTrack.Builder()
+            .setAudioAttributes(AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build())
+            .setAudioFormat(audioFormat)
+            .setBufferSizeInBytes(bufferSizeInBytes)
+            .build()
+
+        // Start capturing audio with AudioRecord
+        audioRecord?.startRecording()
+
+        // Start playing audio with AudioTrack
+        audioTrack?.play()
+
+        // Create a buffer to hold the captured audio data
+        val buffer = ByteArray(bufferSizeInBytes)
+
+        // Continuously capture and play the audio
+        while (true) {
+            // Read the captured audio data
+            val bytesRead = audioRecord?.read(buffer, 0, bufferSizeInBytes)
+
+            if (bytesRead != null && bytesRead > 0) {
+                // Write the captured audio data to the AudioTrack for playback
+                audioTrack?.write(buffer, 0, bytesRead)
+            }
+        }
+    }
+
+    fun stopCapturingAndPlayingAudio() {
+        // Stop capturing audio
+        audioRecord?.stop()
+        audioRecord?.release()
+        audioRecord = null
+
+        // Stop playing audio
+        audioTrack?.stop()
+        audioTrack?.release()
+        audioTrack = null
+    }
+
+
+    private fun startAudioCapture() {
+        // TODO: add code for executing audio capture itself
+        val config = AudioPlaybackCaptureConfiguration.Builder(mediaProjection!!)
+            .addMatchingUsage(AudioAttributes.USAGE_MEDIA)
+            .build()
+
+        val audioFormat = AudioFormat.Builder()
+            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+            .setSampleRate(8000)
+            .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+            .build()
+        Log.d(LOG_TAG,"startAudioCapture");
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECORD_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            return
+        }
+        audioRecord = AudioRecord.Builder()
+            .setAudioFormat(audioFormat)
             .setBufferSizeInBytes(BUFFER_SIZE_IN_BYTES)
             .setAudioPlaybackCaptureConfig(config)
             .build()
 
+
         audioRecord!!.startRecording()
+        Log.d(LOG_TAG,"audioRecord")
 
         audioCaptureThread = thread(start = true) {
-            val outputFile = createAudioFile()
-            Log.d(LOG_TAG, "Created file for capture target: ${outputFile.absolutePath}")
+            val currentDate = Calendar.getInstance().time
+
+            val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val formattedDate = dateFormat.format(currentDate)
+            val outputFile = File(getExternalFilesDir(Environment.DIRECTORY_MUSIC), "${formattedDate}-recording.pcm")
+            Log.d(LOG_TAG, "Created file for capture target: ${outputFile.path}")
             writeAudioToFile(outputFile)
         }
     }
@@ -129,26 +237,61 @@ class MediaCaptureService : Service(){
         val fileName = "Capture-$timestamp.pcm"
         return File(audioCapturesDirectory.absolutePath + "/" + fileName)
     }
-
     private fun writeAudioToFile(outputFile: File) {
-        val fileOutputStream = FileOutputStream(outputFile)
+        val bufferedOutputStream = BufferedOutputStream(FileOutputStream(outputFile))
         val capturedAudioSamples = ShortArray(NUM_SAMPLES_PER_READ)
 
-        while (!audioCaptureThread.isInterrupted) {
-            audioRecord?.read(capturedAudioSamples, 0, NUM_SAMPLES_PER_READ)
+        try {
+            while (!audioCaptureThread.isInterrupted) {
+                audioRecord?.read(capturedAudioSamples, 0, NUM_SAMPLES_PER_READ)
+                val byteArray = capturedAudioSamples.toByteArray()
 
-            fileOutputStream.write(
-                capturedAudioSamples.toByteArray(),
-                0,
-                BUFFER_SIZE_IN_BYTES
-            )
+                // Write the entire buffer at once
+                bufferedOutputStream.write(byteArray)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            // Close the stream
+            bufferedOutputStream.close()
         }
 
-        fileOutputStream.close()
-        Log.d(LOG_TAG, "Audio capture finished for ${outputFile.absolutePath}. File size is ${outputFile.length()} bytes.")
+        Log.d(
+            LOG_TAG,
+            "Audio capture finished for ${outputFile.absolutePath}. File size is ${outputFile.length()} bytes."
+        )
     }
+
+
+//    private fun writeAudioToFile(outputFile: File) {
+//
+//        val fileOutputStream = FileOutputStream(outputFile)
+//
+//        val capturedAudioSamples = ShortArray(NUM_SAMPLES_PER_READ)
+//
+//
+//
+//        while (!audioCaptureThread.isInterrupted) {
+//            audioRecord?.read(capturedAudioSamples, 0, NUM_SAMPLES_PER_READ)
+//
+//            fileOutputStream.write(
+//                capturedAudioSamples.toByteArray(),
+//                0,
+//                BUFFER_SIZE_IN_BYTES
+//            )
+//        }
+//
+//        fileOutputStream.close()
+//        Log.d(
+//            LOG_TAG,
+//            "Audio capture finished for ${outputFile.absolutePath}. File size is ${outputFile.length()} bytes."
+//        )
+//    }
+
     private fun stopAudioCapture() {
         // TODO: Add code for stopping the audio capture
+        Log.d(LOG_TAG,"stopAudioCapture");
+
         requireNotNull(mediaProjection) { "Tried to stop audio capture, but there was no ongoing capture in place!" }
 
         audioCaptureThread.interrupt()
@@ -161,6 +304,7 @@ class MediaCaptureService : Service(){
         mediaProjection!!.stop()
         stopSelf()
     }
+
     private fun ShortArray.toByteArray(): ByteArray {
         val bytes = ByteArray(size * 2)
         for (i in 0 until size) {
@@ -176,13 +320,15 @@ class MediaCaptureService : Service(){
         private const val SERVICE_ID = 123
         private const val NOTIFICATION_CHANNEL_ID = "AudioCapture channel"
 
-        private const val NUM_SAMPLES_PER_READ = 1024
+        private const val NUM_SAMPLES_PER_READ = 4096
         private const val BYTES_PER_SAMPLE = 2 // 2 bytes since we hardcoded the PCM 16-bit format
         private const val BUFFER_SIZE_IN_BYTES = NUM_SAMPLES_PER_READ * BYTES_PER_SAMPLE
 
         const val ACTION_START = "AudioCaptureService:Start"
         const val ACTION_STOP = "AudioCaptureService:Stop"
         const val EXTRA_RESULT_DATA = "AudioCaptureService:Extra:ResultData"
+
+
     }
 
 }
